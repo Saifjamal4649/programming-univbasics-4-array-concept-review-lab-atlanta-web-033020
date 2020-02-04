a6258025028d4c98111f5e72c795d6af5d519f21
Saif Jamal <saifjamal4649@gmail.com>
2020-02-04 03:54:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = [0]
+  max = array[0]
   length = array.length
   length.times do |index|
     if max < array[index]
@@ -22,7 +22,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = [0]
+  min = array[0]
   length = array.length
   length.times do |index|
     if min > array[index]
