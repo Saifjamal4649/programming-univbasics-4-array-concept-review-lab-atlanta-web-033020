2b92a337fa41622f9dc9b0554804b953c22190bf
Saif Jamal <saifjamal4649@gmail.com>
2020-02-04 03:56:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   length = array.length 
   length.times do |index|
     if array[index] == value_to_find
-      return array[index]
+      return index
    
     end
   end
